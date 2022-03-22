bf15a850f1fa539fc5c6fe9f5467b07f2e27b903
Heina <heina2ffect@gmail.com>
2022-03-22 22:05:22 +0900

    Style: 코드 정리

---
+++ main.js
@@ -0,0 +1,101 @@
+
+class Header extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+    <header>
+      <article id="header">
+        <section>
+          <div class="mySelf">
+            <div>
+              <img src="img/heinaImg.png" alt="heinaImg">
+            </div>
+            <div class="introRandom">
+              <p>
+                I'M
+              </p>
+              <p class="textRandom">
+              </p>
+              <p>
+                HYUNA KIM
+              </p>
+            </div>
+          </div>
+          <div class="quotes">
+            <p>
+              "I would rather die of passion than of boredom"
+            </p>
+            <span>
+              Vincent van Gogh
+            </span>
+          </div>
+        </section>
+        <section class="mySelf_txt">
+          <p>
+            안녕하세요
+            <br>
+            저는 Front-end 직군을 희망하는 개발자 입니다.
+            <br>
+            끊임없이 배우고 성장하길 원하고 있습니다.
+            <br>
+          </p>
+          <p>
+            저에 대해 궁금하시거나 관심이 있으시다면, 언제든지 연락주세요 
+            <strong>
+              heina2ffect@gmail.com
+            </strong>    
+          </p>
+          <p>
+            당신과 함께 더 멋진 앞으로를 꿈꾸고 싶습니다.
+            <br>
+            또 언제든지 이곳에 방문하신다면 항상 발전하는 저를 보고 계실겁니다.
+            <br>
+            감사합니다. 다음에 또 뵙겠습니다. 
+          </p>
+          <p>
+            Adios
+          </p>
+        </section>
+      </article>
+      <article id="nav">
+        <ul>
+          <li>
+            <a class="darkMode">
+              <img class="lightBtn" src="img/sun.png" alt="darkLightMode">
+            </a>
+          </li>
+          <li>
+            <a href="./index.html">
+              <!-- <img class="navBtn" src="img/skill.svg" alt="skillSvg"> -->
+              <img class="navBtn" src="img/skill.png" alt="skillSvg">
+              <p class="caption">SKILL</p>
+            </a>
+          </li>
+          <li>
+            <a href="./project.html">
+              <img class="navBtn navDark" src="img/project.svg" alt="proSvg">
+              <p class="caption">PROJECT</p>
+            </a>
+          </li>
+          <li>
+            <a href="./about.html">
+              <img class="navBtn navDark" src="img/monster.svg" alt="carSvg">
+              <p class="caption">ABOUT</p>
+            </a>
+          </li>
+          <li>
+            <a href="./contact.html">
+              <img class="navBtn navDark" src="img/add.svg" alt="addSvg">
+              <p class="caption">CONTACT</p>
+            </a>
+          </li>
+        </ul>
+      </article>
+    </header>
+    `
+  }
+
+
+}
+
+customElements.define('main-left', Header);
+
